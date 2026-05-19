15c2277116abceed69cc6430a9a8993353ce7098
nileshgautam <nilesh.gautam@appventurez.com>
2026-05-19 15:27:17 +0530

    #created to list

--- state-management-demo/src/App.jsx
+++ state-management-demo/src/App.jsx
@@ -1,121 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [newTask, setNewTasks] = useState('');
+  const [tasks, setTasks] = useState([]);
+
+  const toggleTask = (id) => {
+    setTasks((tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    }
+    )))
+
+  }
+
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  }
+
+  const handleAddTask = () => {
+    if (newTask.trim()) {
+
+      const task = {
+        id: Date.now(),
+        text: newTask,
+        completed: false
+      }
+
+      setTasks([...tasks, task]);
+      //for now just clear the input.
+      setNewTasks('');
+    }
+  }
+
+  const totalTask = tasks.length;
+  const totalCompletedTask = tasks.filter(((task) => task.completed)).length;
+  const remainingTasks = totalTask - totalCompletedTask;
+
+  const totalCompletedRatio = totalTask > 0 ? (Math.round((totalCompletedTask / totalTask) * 100)) : 0;
+
+  console.log('current task list', tasks);
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div className='App'>
+      <h1>My Task List</h1>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
+      <div style={
+        {
+          background: '#f0f0f0',
+          padding: '10px 20px',
+          margin: '10 10px',
+          borderRadius: '5px'
+        }
+      }>
+        <h3>Task Statistics</h3>
+        <p>Total Tasks: {totalTask} </p>
+        <p>Completed Tasks: {totalCompletedTask} </p>
+        <p>Remaining Tasks: {remainingTasks} </p>
+        <div style={{
+          width: '100%',
+          height: '20px',
+          borderRadius: '10px',
+          background: '#ddd',
+          overflow: 'hidden',
+        }}>
+          <div
+            style={{
+              width: `${totalCompletedRatio}%`,
+              height: '100%',
+              backgroundColor: '#4caf50',
+              transition: 'width 0.3s ease'
+            }}
+          ></div>
         </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <p>Progress : {totalCompletedRatio} %</p>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      </div>
+
+      <input type="text"
+        value={newTask}
+        placeholder='Enter new task here....'
+        onChange={(e) => setNewTasks(e.target.value)}
+      />
+      <button onClick={handleAddTask}>Add new task</button>
+
+
+
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            <input type="checkbox" checked={task.completed}
+              onChange={() => toggleTask(task.id)}
+            />
+            {task.text}
+            <button onClick={() => deleteTask(task.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
